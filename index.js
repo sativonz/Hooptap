e563018919450010e980a9cd1e8d68c1ee217f49
sativonz <fernandogarciafornes@gmail.com>
2016-06-09 16:29:07 +0200

    Deleted uderscore and commented unnused templates

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 /**
  * Created by fernando on 6/9/16.
  */
+var jade = require('jade');
+
+// compile
+var fn = jade.compile('string of jade', options);
+var html = fn(locals);
+
+// renderFile
+var html = jade.renderFile('templates/jade/marketplace.jade', merge('marketplace', locals));
