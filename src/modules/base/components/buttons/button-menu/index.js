d0a59d7a0f7c992744efd278935653d37b48b03a
Fer-Hooptap <fernandogarciafornes@gmail.com>
2016-07-04 11:33:00 +0200

    Added component view for widget-full;

--- src/modules/base/components/buttons/button-menu/index.js
+++ src/modules/base/components/buttons/button-menu/index.js
@@ -8,10 +8,5 @@ export default() => ({
     template,
     link: (attrs, scope, element) => {
 
-        // $('c-button-init').click(function(ev) {
-        //     $('#ht-widgets').show("slow");
-        //     $('c-button-init').hide(100);
-        // });
-
     }
 });
