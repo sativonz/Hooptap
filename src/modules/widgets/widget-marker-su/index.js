efac0c42f467a5f8ff2a326ec680965a8f570be4
Fer-Hooptap <fernandogarciafornes@gmail.com>
2016-07-05 18:20:26 +0200

    Renaming components

--- src/modules/widgets/widget-marker-su/index.js
+++
@@ -1,6 +0,0 @@
-import angular from 'angular-mod';
-
-let module = angular.module('app.widgetMarkerSu', [])
-    .directive('wWidgetMarkerSu', require('./component').default);
-
-export default module.name;
