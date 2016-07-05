a9b75ead4ca6cfc83e10ca4c9b1faadf5561f3be
Fer-Hooptap <fernandogarciafornes@gmail.com>
2016-07-05 18:32:46 +0200

    ngdocs for buttons

--- src/modules/base/components/buttons/button-menu/index.js
+++ src/modules/base/components/buttons/button-menu/index.js
@@ -1,6 +1,13 @@
 import template from './template.jade';
 import './styles.scss';
-
+/**
+ * @ngdoc directive
+ * @name Button menu
+ * @module Components
+ * @description Button to open the menu.
+ * @restrict E
+ * @element ANY
+ */
 export default() => ({
     restrict: 'E',
     scope: {
