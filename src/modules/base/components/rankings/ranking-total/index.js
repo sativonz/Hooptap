159908d6b9367c5c7ea708e2c29985f43661e6af
Fer-Hooptap <fernandogarciafornes@gmail.com>
2016-07-06 15:31:51 +0200

    Fixed header component in ranking total

--- src/modules/base/components/rankings/ranking-total/index.js
+++ src/modules/base/components/rankings/ranking-total/index.js
@@ -28,6 +28,10 @@ export default($rootScope) => ({
             {desc: 'Los mas malos', rankingType:'malos', img: "http://vignette3.wikia.nocookie.net/alderapedia/images/5/5e/Ranking_icon.png/revision/latest?cb=20140822102748&path-prefix=es"},
             {desc: 'Ranking diario', rankingType:'daily', img: "http://vignette3.wikia.nocookie.net/alderapedia/images/5/5e/Ranking_icon.png/revision/latest?cb=20140822102748&path-prefix=es"},
             {desc: 'Histórico', rankingType:'historicco', img: "http://vignette3.wikia.nocookie.net/alderapedia/images/5/5e/Ranking_icon.png/revision/latest?cb=20140822102748&path-prefix=es"},
+            {desc: 'Histórico', rankingType:'historicco', img: "http://vignette3.wikia.nocookie.net/alderapedia/images/5/5e/Ranking_icon.png/revision/latest?cb=20140822102748&path-prefix=es"},
+            {desc: 'Histórico', rankingType:'historicco', img: "http://vignette3.wikia.nocookie.net/alderapedia/images/5/5e/Ranking_icon.png/revision/latest?cb=20140822102748&path-prefix=es"},
+            {desc: 'Histórico', rankingType:'historicco', img: "http://vignette3.wikia.nocookie.net/alderapedia/images/5/5e/Ranking_icon.png/revision/latest?cb=20140822102748&path-prefix=es"},
+            {desc: 'Histórico', rankingType:'historicco', img: "http://vignette3.wikia.nocookie.net/alderapedia/images/5/5e/Ranking_icon.png/revision/latest?cb=20140822102748&path-prefix=es"},
 
         ];
     },
--- src/modules/widgets/widget-ranking-total/component/index.js
+++ src/modules/widgets/widget-ranking-total/component/index.js
@@ -4,6 +4,7 @@ import template from './template.jade';
 export default() => ({
     restrict: 'E',
     scope: {},
+    transclude: true,
     link: (scope, element, attrs)=>{
 
 
