df179e87db430b084f0de6338cc367f6a0daeaf5
segux <jsegura@hooptap.com>
2016-07-13 18:01:23 +0200

    Added valdr and valdr-message

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import 'angular-storage';
 import 'angular-resource';
 import 'angular-animate';
 import 'valdr';
+import 'valdr/valdr-message';
 import tabs from 'angular-ui-bootstrap/src/tabs';
 import progressBar from 'angular-ui-bootstrap/src/progressbar';
 
@@ -29,8 +30,8 @@ import './common/services/angular-sdk.js';
     angular.module('htWidget',
         [
             //Vendor injectors
-            'ngResource',   /*angular resources*/
-            'ngAnimate',    /*angular animate*/
+            'ngResource', /*angular resources*/
+            'ngAnimate', /*angular animate*/
             tabs,
             progressBar,
             'valdr',
@@ -71,16 +72,181 @@ import './common/services/angular-sdk.js';
 
 
     //Controller for widget
-        .controller('htWidgetCtrl', ($scope, $compile, Admin, Badge)=> {
+        .controller('htWidgetCtrl', ($q, $scope, $compile, Event, Customer, Admin, Rule)=> {
+
             //hooptapSDK object for sdk on javascript
-            window.hooptapSDK = {};
-            window.hooptapSDK.Admin = Admin;
-            window.hooptapSDK.Badge = Badge;
+
+            var TEST = true;
+
+            window.Hooptap = function (a0, a1, a2, a3) {
+                if (typeof a0 == 'object' && Object.keys(a0).length) {
+                    return window.Hooptap.sendEvent(a0);
+                }
+                else if (typeof a0 === 'string') {
+                    var params = {category: a0};
+                    if (a1 && typeof a1 == 'string') params.action = a1;
+                    if (a2 && typeof a2 == 'string') params.label = a2;
+                    if (a3 && typeof a3 == 'string' || typeof a3 == 'number') params.value = '' + a3;
+
+                    return window.Hooptap.sendEvent(params);
+                }
+                else {
+                    return false;
+                }
+
+            };
+
+            window.Hooptap.sendEvent = function (params) {
+                var typeOf = {
+                    "category": "string",
+                    "action": "string",
+                    "label": "string",
+                    "value": "string"
+                    //"customerId": "string"
+                };
+
+                if ('object' !== typeof params || params == null || Array.isArray(params) || !Object.keys(params).length
+                )
+                    return false;
+
+                var valid = {}, failed = {};
+                for (var param in params) {
+                    if (typeOf[param] == typeof params[param])
+                        valid[param] = params[param];
+                    else
+                        failed[param] = params[param];
+                }
+
+                if (Object.keys(failed).length)
+                    return false;
+                else
+                    return Event.create(params);
+
+            };
+
+            window.Hooptap.loginCustomer = function (params) {
+                if (params === 'test')
+                    params = {email: 'pepe@pepe.com', password: 'pepe'};
+
+                if (arguments.length && typeof params == 'object' && Object.keys(params).length) {
+                    debugger;
+                    var token = false;
+
+                    if (Customer.isAuthenticated && Customer.isAuthenticated())
+                        Customer.logout();
+                    if (Admin.isAuthenticated && Admin.isAuthenticated())
+                        Admin.logout();
+
+                    token = Customer.login(params)
+                        .$promise
+                        .then((response) => {
+                            //debugger;
+                            console.log('response', response);
+                        })
+                        .catch((e) => {
+                            let errors = {
+                                '-1': 'errors.sdk.noInternet',
+                                '401': 'errors.sdk.badLogin',
+                                '500': 'errors.sdk.noInternet'
+                            };
+                            console.warn('Login error:', e.status);
+                            console.warn('Login error:', errors[e.status]);
+                        });
+                    return token;
+                }
+                else
+                    return false;
+            };
+
+            if (TEST) {
+                Object.assign(
+                    window.Hooptap,
+                    {
+                        Admin: Admin,
+                        Customer: Customer,
+                        Event: Event,
+                        Rule: Rule
+                    }
+                );
+
+                window.Hooptap.loginAdmin = function (params) {
+                    if (params === 'test') {
+                        params = {email: 'admin@hooptap.com', password: 'hooptap'};
+
+                        if (Customer.isAuthenticated && Customer.isAuthenticated())
+                            Customer.logout();
+                        if (Admin.isAuthenticated && Admin.isAuthenticated())
+                            Admin.logout();
+
+                        return Admin.login(params);
+                    }
+                    else
+                        return false;
+                };
+
+                window.Hooptap.test = function (functionName) { //
+                    var tests = {
+                        'loginCustomer': {
+                            "email": "pepe@pepe.com",
+                            "password": "pepe"
+                        },
+                        'sendEvent': [
+                            // boolean
+                            false,
+                            // number
+                            999,
+                            // string
+                            'word',
+                            // array
+                            [],
+                            // empty object
+                            {},
+                            // insufficient object
+                            {"what": false},
+                            // invalid param value type
+                            {"category": false},
+
+                            // valid param value type
+                            {"category": 'false'}
+                        ]
+                    };
+
+                    function testFunction(name) {
+                        var Tries = tests[name];
+
+                        for (var Try in Tries) {
+                            console.log('\ntry:' + Try + ' ' + name + ' with ' + typeof Tries[Try], Tries[Try]);
+                            console.log('results:', window.Hooptap[name](Tries[Try]));
+                        }
+                    }
+
+                    if (functionName && typeof functionName == 'string' && typeof window.Hooptap[functionName] == 'function') {
+                        var _tests = tests[functionName];
+                        tests = {};
+                        tests[functionName] = _tests;
+                    }
+                    else if (functionName)
+                        tests = {};
+
+                    if (Object.keys(tests).length) {
+                        for (var test in tests) {
+                            testFunction(test);
+                        }
+                    }
+                    else
+                        return false;
+
+                };
+
+
+                $compile(angular.element('#htroot').html())($scope);
+
+            }
             //Example of widget render
             //console.log(angular.element('#ht-widget').html());
             $compile(angular.element('#ht-widget').html())($scope);
             // angular.element('#htWidget').append('<widget1></widget1>');
-       
+
 
         })
         .config(require('./config').default)
--- src/modules/widgets/widget-full/config.js
+++ src/modules/widgets/widget-full/config.js
@@ -1,14 +1,9 @@
-export default (valdrProvider)=>{
+export default (valdrProvider)=> {
     valdrProvider.addConstraints({
         'Customer': {
-            'customerEmail': {
-                'size': {
-                    'min': 3,
-                    'max': 20,
-                    'message': 'First name is required to be between 3 and 20 characters.'
-                },
-                'required': {
-                    'message': 'First name is required.'
+            'email': {
+                'email': {
+                    'message': 'Must be a valid email.'
                 }
             }
         }
