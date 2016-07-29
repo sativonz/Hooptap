e66f2be9fe68d08b0aa8972a8e5e9f5012814f6e
Sativonz <fernandogarciafornes@gmail.com>
2016-07-29 14:38:08 +0200

    Global activity

--- src/modules/components/game/game-global-feed/index.js
+++ src/modules/components/game/game-global-feed/index.js
@@ -3,11 +3,47 @@ import './styles.scss';
 //TODO ngDocs
 export default($timeout) => ({
     restrict: 'E',
-    scope: {
-    },
+    template,
+    scope: {},
     link: (scope, element, attrs)=> {
-
-    },
-    template
+        scope.activity = {
+            "global": {
+                "name": "Actividad global",
+                "img": "http://www.artecnics.com/img/inicio/pasafotos/circuloazul.png",
+                "feed": [
+                    {
+                        "title": "Has ganado un badge: Jugón",
+                        "date": "22/12/2015",
+                        "img": "https://www.uts.edu.au/sites/default/files/Andy%20So_circle.jpg",
+                    },
+                    {
+                        "title": "Has ganado 1000 pts",
+                        "date": "5/1/2016",
+                        "img": "http://icons.iconarchive.com/icons/seanau/fresh-web/512/Badge-icon.png",
+                    },
+                    {
+                        "title": "Has ganado 2 diamantes",
+                        "date": "22/05/2016",
+                        "img": "https://www.uts.edu.au/sites/default/files/Andy%20So_circle.jpg",
+                    },
+                    {
+                        "title": "Has ganado 500 monedas",
+                        "date": "21/02/2016",
+                        "img": "https://www.uts.edu.au/sites/default/files/Andy%20So_circle.jpg",
+                    },
+                    {
+                        "title": "Has ganado 4 estrellas",
+                        "date": "12/09/2015",
+                        "img": "https://www.uts.edu.au/sites/default/files/Andy%20So_circle.jpg",
+                    },
+                    {
+                        "title": "Has ganado 2500 pts",
+                        "date": "2/05/2016",
+                        "img": "https://www.uts.edu.au/sites/default/files/Andy%20So_circle.jpg",
+                    }
+                ],
+            },
+        };
+    }
 
 });
--- src/modules/components/rankings/ranking-list/index.js
+++ src/modules/components/rankings/ranking-list/index.js
@@ -62,19 +62,6 @@ export default() => ({
         };
 
 
-        scope.items = [
-            {position: '2', username: 'sat1', score: 1000},
-            {position: '1', username: 'jos1', score: 5000},
-            {position: '2', username: 'blan', score: 3000},
-            {position: '4', username: 'vonz', score: 4500},
-            {position: '22', username: 'sergux', score: 200},
-            {position: '12', username: 'korsakof', score: 100},
-            {position: '10', username: 'paul', score: 500},
-            {position: '5', username: 'stalck', score: 2540},
-            {position: '9', username: 'atpc', score: 1254}
-
-        ];
-
 
     },
 
