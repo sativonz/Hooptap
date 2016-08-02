faf626518f6d8e370d0aec59a36b4f0639ec776c
Sativonz <fernandogarciafornes@gmail.com>
2016-08-02 13:14:36 +0200

    ~

--- src/modules/components/game/game-wrapper/index.js
+++ src/modules/components/game/game-wrapper/index.js
@@ -6,36 +6,37 @@ import test from '../../quests/quests-list/template.jade';
 //TODO ngDocs
 export default($uibModal, $log) => ({
     restrict: 'E',
+    template,
     scope: {
     },
     link: (scope, element, attrs)=> {
-
+        //TODO por hacer el titulo
         scope.open = function (size) {
+            // var svgTag = angular.element('<svg width="600" height="100" class="svg"></svg>');
+            // var modalContent = angular.element(document.querySelector( '.modal-content' )
+            // angular.element(modalContent).appendTo('Hi');
 
             var modalInstance = $uibModal.open({
                 animation: scope.animationsEnabled,
                 template: test,
                 controller: 'htWidgetCtrl',
                 size: size,
-                resolve: {
-                    items: function () {
-                        return scope.items;
-                    }
-                }
             });
 
+            $('.modal-content').append('Hi');
 
-        };
 
-        scope.ok = function () {
-            $uibModalInstance.close($scope.selected.item);
-        };
+            //-> UNUSED
+            // scope.ok = function () {
+            //     $uibModalInstance.close($scope.selected.item);
+            // };
+            //
+            // scope.cancel = function () {
+            //     $uibModalInstance.dismiss('cancel');
+            // };
 
-        scope.cancel = function () {
-            $uibModalInstance.dismiss('cancel');
-        };
+        }
 
-    },
-    template
+    }
 
 });
