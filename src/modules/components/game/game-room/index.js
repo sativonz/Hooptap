24ea7d29de58cc9d1706acfa8044e4c2bd728cac
Sativonz <fernandogarciafornes@gmail.com>
2016-08-03 11:22:44 +0200

    Added in gameroom custom image and default image background

--- src/modules/components/game/game-room/index.js
+++ src/modules/components/game/game-room/index.js
@@ -5,18 +5,22 @@ export default($timeout) => ({
     restrict: 'E',
     template,
     scope: {
+        showImage: '=',
+        showTitle: '=',
+        showDesc: '=',
     },
     link: (scope, element, attrs)=> {
 
         //Definir el numero de cols
-        scope.numberCols = 5;
-
+        scope.numberCols = 2;
+        scope.defaultImage = "http://hooptap.s3.amazonaws.com/widgets/icono-default.png";
         scope.gameroom = [
             {
                 "id": "5775397981dbc14a04530f73",
-                "name": "Título",
-                "title": "Título",
-                "desc": "Descripción",
+                "name": "Colin McRae",
+                "title": "Colin McRae",
+                "desc": "Campeonato WRC",
+                "customImage": "http://www.cochesgt.com/wp-content/uploads/2015/09/Subaru-Impreza-Colin-McRae-2-750x400.jpg",
                 "img" :  "http://hooptap.s3.amazonaws.com/widgets/icono-default.png",
             },
             {
@@ -24,6 +28,7 @@ export default($timeout) => ({
                 "name": "Título",
                 "title": "Título",
                 "desc": "Descripción",
+                "customImage": "",
                 "img" :  "http://hooptap.s3.amazonaws.com/widgets/icono-default.png",
             },
             {
@@ -31,6 +36,7 @@ export default($timeout) => ({
                 "name": "Título",
                 "title": "Título",
                 "desc": "Descripción",
+                "customImage": "",
                 "img" :  "http://hooptap.s3.amazonaws.com/widgets/icono-default.png",
             },
             {
@@ -38,6 +44,7 @@ export default($timeout) => ({
                 "name": "Título",
                 "title": "Título",
                 "desc": "Descripción",
+                "customImage": "",
                 "img" :  "http://hooptap.s3.amazonaws.com/widgets/icono-default.png",
             },
             {
@@ -45,6 +52,7 @@ export default($timeout) => ({
                 "name": "Título",
                 "title": "Título",
                 "desc": "Descripción",
+                "customImage": "",
                 "img" :  "http://hooptap.s3.amazonaws.com/widgets/icono-default.png",
             }
         ];
