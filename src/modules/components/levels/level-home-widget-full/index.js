2f1337f1e1defa90ddb7374abe03c8b87464ee44
sativonz <fernandogarciafornes@gmail.com>
2016-08-10 18:11:27 +0200

    create component for level home in widget full

---
+++ src/modules/components/levels/level-home-widget-full/index.js
@@ -0,0 +1,26 @@
+import template from './template.jade';
+import './styles.scss';
+/**
+ * @ngdoc directive
+ * @name Level home widget full
+ * @module Components
+ * @description Component for see the actual level of the user and details of the next level
+ * @restrict E
+ * //TODO revisar params
+ * @param {String} type Name of the state in which the user is at the current level
+ * @param {Int} dynamic The numerical value of 200, of the user's progress at the current level
+ * @param {Image} image Image of the actual level
+ * @element ANY
+ */
+export default(Customer, LoopBackAuth, $rootScope) => ({
+    restrict: 'E',
+    template,
+    scope: {
+        showProgressBar: '=',
+    },
+    transclude: true,
+    link: (scope, element, attrs)=> {
+
+    }
+
+});
