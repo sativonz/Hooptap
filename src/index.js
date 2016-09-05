6bd7972b4e3892e225592d8d8c45da202518c5eb
segux <djsegusg@example.com>
2016-09-05 18:05:15 +0200

    Added query selector

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ import './common/services/angular-sdk.js';
 (function () {
     angular.module('Hooptap',
         [
-            
+
             //Vendor injectors
             'ngResource', /*angular resources*/
             'ngAnimate', /*angular animate*/
@@ -57,26 +57,21 @@ import './common/services/angular-sdk.js';
             //hooptapSDK object for sdk on javascript
             var TEST = false;
 
-            function processTriggers( collection )
-            {
-                console.log( this.name, collection );
+            function processTriggers(collection) {
+                console.log(this.name, collection);
 
-                for( var key in collection )
-                {
-                    if ( typeof processTriggers[trigger.data.model] == 'function' )
-                    {
-                        processTriggers[trigger.data.model]( collection[key] );
+                for (var key in collection) {
+                    if (typeof processTriggers[trigger.data.model] == 'function') {
+                        processTriggers[trigger.data.model](collection[key]);
                     }
                 }
-
 
 
                 return this;
             }
 
-            processTriggers.ScoreUnit = function(trigger)
-            {
-                console.log( this.name );
+            processTriggers.ScoreUnit = function (trigger) {
+                console.log(this.name);
                 alert(
                     [
                         trigger.action,
@@ -87,84 +82,80 @@ import './common/services/angular-sdk.js';
                 );
             };
 
-            window.Hooptap = function( a0, a1, a2, a3 )
-            {
-                if ( typeof a0 == 'object' && Object.keys( a0 ).length )
-                {
+            window.Hooptap = function (a0, a1, a2, a3) {
+                if (typeof a0 == 'object' && Object.keys(a0).length) {
                     return window.Hooptap.sendEvent(a0);
                 }
-                else if ( typeof a0 === 'string' )
-                {
-                    var params = { category: a0 };
-                    if ( a1 && typeof a1 == 'string' ) params.action = a1;
-                    if ( a2 && typeof a2 == 'string' ) params.label = a2;
-                    if ( a3 && typeof a3 == 'string' || typeof a3 == 'number' ) params.value = ''+a3;
+                else if (typeof a0 === 'string') {
+                    var params = {category: a0};
+                    if (a1 && typeof a1 == 'string') params.action = a1;
+                    if (a2 && typeof a2 == 'string') params.label = a2;
+                    if (a3 && typeof a3 == 'string' || typeof a3 == 'number') params.value = '' + a3;
 
-                    return window.Hooptap.sendEvent( params );
+                    return window.Hooptap.sendEvent(params);
                 }
-                else
-                {
+                else {
                     return false;
                 }
 
             };
-            window.Hooptap.api = ( model, method, params, _then, _catch ) => {
+            window.Hooptap.api = (model, method, params, _then, _catch) => {
                 // TODO improve Hooptap.api method
 
                 model = $injector.get(model);
                 method = model[method];
 
-                let promise = method( params ).$promise;
-                if(!promise) {
-                    return method( params );
+                let promise = method(params).$promise;
+                if (!promise) {
+                    return method(params);
                 }
-                if(_then) promise.then( (response) => { _then(response) } );
-                if(_catch) promise.catch( (error) => { _catch(error) } );
+                if (_then) promise.then((response) => {
+                    _then(response)
+                });
+                if (_catch) promise.catch((error) => {
+                    _catch(error)
+                });
 
                 return promise;
             };
 
-            window.Hooptap.sendEvent = function( params )
-            {
+            window.Hooptap.sendEvent = function (params) {
 
                 var typeOf = {
-                    "category"  : "string",
-                    "action"    : "string",
-                    "label"     : "string",
-                    "value"     : "string"
+                    "category": "string",
+                    "action": "string",
+                    "label": "string",
+                    "value": "string"
                     //"customerId": "string"
                 };
 
-                if ( 'object' !== typeof params || params == null || Array.isArray(params) || !Object.keys(params).length
+                if ('object' !== typeof params || params == null || Array.isArray(params) || !Object.keys(params).length
                 )
                     return false;
 
                 var valid = {}, failed = {};
-                for( var param in params )
-                {
-                    if ( typeOf[param] == typeof params[param] )
+                for (var param in params) {
+                    if (typeOf[param] == typeof params[param])
                         valid[param] = params[param];
                     else
                         failed[param] = params[param];
                 }
 
-                if ( Object.keys(failed).length )
+                if (Object.keys(failed).length)
                     return false;
-                else
-                {
-                    return Event.create( params ).$promise
+                else {
+                    return Event.create(params).$promise
                         .then(response => {
 
                             let thisEventId = response.id;
                             let ctxTriggers = response._context.events[thisEventId].triggers;
 
-                            if(ctxTriggers && typeof ctxTriggers == 'object' && Object.keys( ctxTriggers ))
-                            {
-                                console.log( 'EventTriggers', ctxTriggers );
+                            if (ctxTriggers && typeof ctxTriggers == 'object' && Object.keys(ctxTriggers)) {
+                                console.log('EventTriggers', ctxTriggers);
                                 //processTriggers(ctxTriggers);
                             }
                             else
-                                console.log( 'No Event Triggers' );
+                                console.log('No Event Triggers');
 
                             return ctxTriggers;
                         })
@@ -176,37 +167,33 @@ import './common/services/angular-sdk.js';
             };
 
 
-            window.Hooptap.loginCustomer = function( params )
-            {
+            window.Hooptap.loginCustomer = function (params) {
                 if (params === 'test')
-                    params = { email: 'pepe@pepe.com', password: 'pepe', productId: 'test' };
+                    params = {email: 'pepe@pepe.com', password: 'pepe', productId: 'test'};
 
-                if ( arguments.length && typeof params == 'object' && Object.keys(params).length )
-                {
+                if (arguments.length && typeof params == 'object' && Object.keys(params).length) {
                     debugger;
                     var token = false;
 
-                    if ( Customer.isAuthenticated() )
+                    if (Customer.isAuthenticated())
                         Customer.logout();
-                    if ( Admin.isAuthenticated() )
+                    if (Admin.isAuthenticated())
                         Admin.logout();
 
-                    token = Customer.login( params )
+                    token = Customer.login(params)
                         .$promise
-                        .then( (response) =>
-                        {
+                        .then((response) => {
                             //debugger;
-                            console.log( 'response', response );
+                            console.log('response', response);
                         })
-                        .catch( (e) =>
-                        {
+                        .catch((e) => {
                             let errors = {
                                 '-1': 'errors.sdk.noInternet',
                                 '401': 'errors.sdk.badLogin',
                                 '500': 'errors.sdk.noInternet'
                             };
-                            console.warn( 'Login error:', e.status );
-                            console.warn( 'Login error:', errors[e.status] );
+                            console.warn('Login error:', e.status);
+                            console.warn('Login error:', errors[e.status]);
                         });
                     return token;
                 }
@@ -214,17 +201,15 @@ import './common/services/angular-sdk.js';
                     return false;
             };
 
-            if(TEST)
-            {
+            if (TEST) {
 
-                window.Hooptap.loginAdmin = function( params )
-                {
+                window.Hooptap.loginAdmin = function (params) {
                     if (params === 'test') {
                         params = {email: 'admin@hooptap.com', password: 'hooptap'};
 
-                        if ( Customer.isAuthenticated() )
+                        if (Customer.isAuthenticated())
                             Customer.logout();
-                        if ( Admin.isAuthenticated() )
+                        if (Admin.isAuthenticated())
                             Admin.logout();
 
                         return Admin.login(params);
@@ -233,12 +218,11 @@ import './common/services/angular-sdk.js';
                         return false;
                 };
 
-                window.Hooptap.test = function( functionName )
-                {
+                window.Hooptap.test = function (functionName) {
                     var tests = {
                         'loginCustomer': {
-                            "email":"pepe@pepe.com",
-                            "password":"pepe"
+                            "email": "pepe@pepe.com",
+                            "password": "pepe"
                         },
                         'sendEvent': [
                             // boolean
@@ -252,40 +236,35 @@ import './common/services/angular-sdk.js';
                             // empty object
                             {},
                             // insufficient object
-                            { "what": false },
+                            {"what": false},
                             // invalid param value type
-                            { "category": false },
+                            {"category": false},
 
                             // valid param value type
-                            { "category": 'false' }
+                            {"category": 'false'}
                         ]
                     };
 
-                    function testFunction( name )
-                    {
-                        var Tries = tests[ name ];
+                    function testFunction(name) {
+                        var Tries = tests[name];
 
-                        for( var Try in Tries)
-                        {
-                            console.log( '\ntry:' + Try + ' ' + name + ' with ' + typeof Tries[Try], Tries[Try] );
-                            console.log( 'results:', window.Hooptap[ name ]( Tries[Try] ) );
+                        for (var Try in Tries) {
+                            console.log('\ntry:' + Try + ' ' + name + ' with ' + typeof Tries[Try], Tries[Try]);
+                            console.log('results:', window.Hooptap[name](Tries[Try]));
                         }
                     }
 
-                    if ( functionName && typeof functionName == 'string' && typeof window.Hooptap[functionName] == 'function')
-                    {
-                        var _tests = tests[ functionName ];
+                    if (functionName && typeof functionName == 'string' && typeof window.Hooptap[functionName] == 'function') {
+                        var _tests = tests[functionName];
                         tests = {};
-                        tests[ functionName ] = _tests;
+                        tests[functionName] = _tests;
                     }
-                    else if ( functionName )
+                    else if (functionName)
                         tests = {};
 
-                    if ( Object.keys( tests ).length )
-                    {
-                        for( var test in tests )
-                        {
-                            testFunction( test );
+                    if (Object.keys(tests).length) {
+                        for (var test in tests) {
+                            testFunction(test);
                         }
                     }
                     else
@@ -295,12 +274,10 @@ import './common/services/angular-sdk.js';
             }
 
 
-
-
         })
-        .directive('createRouter', function($rootScope) {
+        .directive('createRouter', function ($rootScope) {
 
-            $rootScope.changeView = function(routerTargetName, viewName) {
+            $rootScope.changeView = function (routerTargetName, viewName) {
                 if (!viewName) {
                     viewName = routerTargetName;
                     routerTargetName = routerName;
@@ -318,11 +295,11 @@ import './common/services/angular-sdk.js';
             $rootScope.routerChangeFunctions = {};
             return {
                 restrict: 'A',
-                controller: function($scope, $attrs) {
+                controller: function ($scope, $attrs) {
                     let routerName = $attrs.routerName;
                     $scope.view = null;
                     $scope.historic = [];
-                    $scope.back = function(routerTargetName, viewName) {
+                    $scope.back = function (routerTargetName, viewName) {
                         if (!viewName) {
                             viewName = routerTargetName;
                             routerTargetName = routerName;
@@ -341,7 +318,7 @@ import './common/services/angular-sdk.js';
                         console.log($scope.historic);
                         $scope.view = $scope.historic.pop();
                     };
-                    $scope.changeView = function(routerTargetName, viewName) {
+                    $scope.changeView = function (routerTargetName, viewName) {
 
                         if (!viewName) {
                             viewName = routerTargetName;
@@ -361,11 +338,11 @@ import './common/services/angular-sdk.js';
                         }
 
                     };
-                    $rootScope.routerChangeFunctions[routerName] = function() {
+                    $rootScope.routerChangeFunctions[routerName] = function () {
                         return $scope.changeView.apply($scope, arguments);
                     };
 
-                    $scope.isView = function(viewName) {
+                    $scope.isView = function (viewName) {
                         return viewName == $scope.view;
                     }
                 }
@@ -374,11 +351,10 @@ import './common/services/angular-sdk.js';
         .config(require('./config').default)
         .run(require('./boot').default);
 
-        window.Hooptap = {};
-        window.Hooptap.startIn = function ($selector){
-            angular.bootstrap(document.getElementsByTagName($selector), ['Hooptap']);
-        };
+    window.Hooptap = {};
+    window.Hooptap.startIn = function ($selector) {
+        angular.bootstrap(document.querySelector($selector), ['Hooptap']);
+    };
 
-    
 
 }());
